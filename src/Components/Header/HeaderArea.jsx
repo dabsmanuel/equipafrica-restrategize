caf22e8843a81d780837e6210b7d8e88e2e9265d
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-11-19 22:51:46 +0100

    changed button text

--- src/Components/Header/HeaderArea.jsx
+++ src/Components/Header/HeaderArea.jsx
@@ -9,11 +9,9 @@ const Header = () => {
           <span className="green font-bold">Equip Africa</span> | Restrategize
           2022
         </h1>
-        <p className="text-xl py-10 text-center">
-          Join us in for a 3-days Retreat
-        </p>
-        <button class="bg-transparent text-white hover:text-white-400 py-4 px-4 border border-white-500 hover:border-grey-700 rounded text-xl md:max-w-4xl">
-          REGISTER FOR THE RETREAT
+        <p className="text-xl py-10 text-center">It's Time To Retreat!!!</p>
+        <button class="bg-transparent text-white hover:text-white-400 py-4 px-4 border border-white-500 hover:border-grey-700 rounded text-lg md:max-w-4xl">
+          REGISTER FOR RESTRATEGIZE 2022
         </button>
       </div>
     </section>
