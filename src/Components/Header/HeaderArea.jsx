fd5a9b7e09ded3074b2eb191e67b4885aa8a6988
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-11-23 18:45:19 +0100

    changed theme color to gradient

--- src/Components/Header/HeaderArea.jsx
+++ src/Components/Header/HeaderArea.jsx
@@ -7,16 +7,16 @@ const Header = () => {
       className="mx-auto bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white pb-32"
       id="home"
     >
-      <div className="container lg:text-5xl pt-56 typewriter text-center">
-        <h1 className="lg:text-6xl text-xl">
+      <div className="container  pt-56 typewriter text-center">
+        <h1 className="lg:text-5xl text-xl">
           <span className="green font-bold">Equip Africa</span> | Restrategize
           2022
         </h1>
-        <p className="text-xl py-10 text-center tracking-[.28rem]">
+        <p className="text-2xl py-10 text-center tracking-[.28rem]">
           It's Time To Retreat!!!
         </p>
-        <button className="bg-transparent hover:bg-gray-200 text-white hover:text-black py-2 px-4 border border-white-500 hover:border-grey-700 rounded text-base md:max-w-2xl border-2 font-semibold">
-          REGISTER FOR RESTRATEGIZE 2022
+        <button className="bg-transparent hover:bg-gray-200 text-white hover:text-black py-2 px-4 border border-white-500 hover:border-grey-700 rounded text-base md:max-w-2xl border-2 font-semibold tracking-[.3rem]">
+          RESTRATEGIZE 2022
         </button>
       </div>
     </section>
